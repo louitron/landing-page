6a433fe1716732a574d6fb6594d88860537c2210
louitron <luisgarcia1437@gmail.com>
2023-03-05 20:10:44 +0000

    Worked on card more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,13 @@ import { gsap } from 'gsap';
 
 export function Navbar() {
   return (
+    
     <div>
       <nav className="flex bg-slate-700 h-24 justify-between items-center fixed top-0 w-full z-50">
         <div className="text-white">
           <p className="flex text-xl pl-4">Made with React and TailwindCSS</p>
         </div>
-        <div className="flex text-4xl text-white">
+        <div id="logos" className="flex text-4xl text-white">
           <FaReact /> <SiTailwindcss />
         </div>
         <div className="text-gray-500 text-xl">
