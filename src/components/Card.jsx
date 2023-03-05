053dcdf39ed6156564f27cff28f6c1b4b34d100e
louitron <luisgarcia1437@gmail.com>
2023-03-05 00:29:27 +0000

    finished layot for cards, working on footer and overall design improvements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,26 +3,99 @@ import React from "react";
 export function Card() {
   return (
     <div className="flex justify-between mx-10">
-      <div>
-        <div className="bg-white h-[450px] w-[325px] border rounded-md">
-          <div className="h-1/2 bg-gray-400">
-            <img src="" alt="" />
+      <div className="hover:scale-105">
+        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg ">
+          <div className="h-1/2 bg-gray-400 rounded-t-lg">
+            <img className="rounded-t-md" src="#" alt="" />
           </div>
-          <div className="border h-1/2">
-            <h1>Card Title</h1>
-            <p>
+          <div className="flex flex-col justify-start text-center h-1/2">
+            <h1 className="text-3xl my-5">Card Title</h1>
+            <p className="text-xl">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
           </div>
         </div>
-        <div className="h-[50px] w-[325px]">
-          <button type="button">Find Out more!</button>
+        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
+        <button
+          className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+        >
+            Find Out more!
+          </button>
         </div>
+        
       </div>
-      <div className="bg-red-500 h-[500px] w-[325px]"></div>
-      <div className="bg-red-500 h-[500px] w-[325px]"></div>
-      <div className="bg-red-500 h-[500px] w-[325px]"></div>
+      <div className="hover:scale-105">
+        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg ">
+          <div className="h-1/2 bg-gray-400 rounded-t-lg">
+            <img className="rounded-t-md" src="#" alt="" />
+          </div>
+          <div className="flex flex-col justify-start text-center h-1/2">
+            <h1 className="text-3xl my-5">Card Title</h1>
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
+        <button
+          className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+        >
+            Find Out more!
+          </button>
+        </div>
+        
+      </div>
+      <div className="hover:scale-105">
+        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg ">
+          <div className="h-1/2 bg-gray-400 rounded-t-lg">
+            <img className="rounded-t-md" src="#" alt="" />
+          </div>
+          <div className="flex flex-col justify-start text-center h-1/2">
+            <h1 className="text-3xl my-5">Card Title</h1>
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
+        <button
+          className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+        >
+            Find Out more!
+          </button>
+        </div>
+        
+      </div>
+      <div className="hover:scale-105">
+        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg ">
+          <div className="h-1/2 bg-gray-400 rounded-t-lg">
+            <img className="rounded-t-md" src="#" alt="" />
+          </div>
+          <div className="flex flex-col justify-start text-center h-1/2">
+            <h1 className="text-3xl my-5">Card Title</h1>
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
+        <button
+          className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+        >
+            Find Out more!
+          </button>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Navbar } from "@/components/Navbar";
 import { Jumbotron } from "@/components/Jumbotron";
 import { Card } from "@/components/Card";
+import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
     <div className="bg-white">
       <Navbar />
       <Jumbotron />
       <Card />
+      <Footer />
       <h1 className="text-xl justify-end">Testing</h1>
     </div>
   );
