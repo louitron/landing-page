c2f3509f56a60d0d454cba087cc4f84d3886567a
louitron <luisgarcia1437@gmail.com>
2023-03-06 21:36:28 +0000

    Working on implementing responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaReact } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
-import { gsap } from "gsap";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <nav className="flex bg-gradient-to-b from-slate-700 to-blue-400 h-24 justify-between items-center fixed top-0 w-full z-50">
@@ -18,7 +25,7 @@ export function Navbar() {
             <SiTailwindcss />
           </div>
         </div>
-        <div className="text-gray-200 text-xl">
+        <div className="text-gray-200 text-xl hidden md:block">
           <a className="pl-4 hover:text-white" href="#">
             Home
           </a>
@@ -32,7 +39,58 @@ export function Navbar() {
             Contact
           </a>
         </div>
+        <div className="md:hidden flex">
+          {isOpen ? (
+            <button
+              className="text-white text-3xl mr-4"
+              onClick={handleMenuClick}
+            >
+              <AiOutlineClose />
+            </button>
+          ) : (
+            <button
+              className="text-white text-3xl mr-4"
+              onClick={handleMenuClick}
+            >
+              <GiHamburgerMenu />
+            </button>
+          )}
+        </div>
       </nav>
+      {isOpen && (
+        <div className="absolute top-0 left-0 w-full bg-gradient-to-b from-slate-700 to-blue-400">
+          <div className="flex flex-col items-center py-4">
+            <a
+              className="py-2 px-4 text-gray-200 hover:bg-blue-500"
+              href="#"
+              onClick={handleMenuClick}
+            >
+              Home
+            </a>
+            <a
+              className="py-2 px-4 text-gray-200 hover:bg-blue-500"
+              href="#"
+              onClick={handleMenuClick}
+            >
+              About
+            </a>
+            <a
+              className="py-2 px-4 text-gray-200 hover:bg-blue-500"
+              href="#"
+              onClick={handleMenuClick}
+            >
+              Services
+            </a>
+            <a
+              className="py-2 px-4 text-gray-200 hover:bg-blue-500"
+              href="#"
+              onClick={handleMenuClick}
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
