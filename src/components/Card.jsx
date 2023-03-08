35338e51aae0ce5eae5b14ae54aa0bdd17c21dcf
louitron <luisgarcia1437@gmail.com>
2023-03-08 02:31:35 +0000

    refactored card component to use props, added mapping to generate 4 cards with different info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,95 +1,28 @@
 import React from "react";
 
-export function Card() {
+export function Card({ title, bio, imageURL }) {
   return (
-    <div className=" flex flex-wrap justify-center">
-      <div className="hover:scale-105 mx-10 my-5 ">
-        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg">
-          <div className="h-1/2 bg-gray-400 rounded-t-lg">
-            <img className="rounded-t-md" src="#" alt="" />
-          </div>
-          <div className="flex flex-col justify-start text-center h-1/2">
-            <h1 className="text-3xl my-5">Card Title</h1>
-            <p className="text-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
+    <div className="hover:scale-105 mx-4 my-5 ">
+      <div className="bg-white h-[450px] w-[325px] border rounded-t-lg">
+        <div className=" h-1/2 bg-gray-400 rounded-t-lg">
+          <img
+            className="h-full w-full object-cover rounded-t-md"
+            src={imageURL}
+            alt=""
+          />
         </div>
-        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
-          <button
-            className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="button"
-          >
-            Find Out more!
-          </button>
+        <div className="flex flex-col justify-start text-center h-1/2">
+          <h1 className="bg-white text-3xl my-5">{title}</h1>
+          <p className="bg-white text-xl">{bio}</p>
         </div>
       </div>
-      <div className="hover:scale-105 mx-10 my-5">
-        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg">
-          <div className="h-1/2 bg-gray-400 rounded-t-lg">
-            <img className="rounded-t-md" src="#" alt="" />
-          </div>
-          <div className="flex flex-col justify-start text-center h-1/2">
-            <h1 className="text-3xl my-5">Card Title</h1>
-            <p className="text-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-        </div>
-        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
-          <button
-            className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="button"
-          >
-            Find Out more!
-          </button>
-        </div>
-      </div>
-      <div className="hover:scale-105 mx-10 my-5">
-        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg">
-          <div className="h-1/2 bg-gray-400 rounded-t-lg">
-            <img className="rounded-t-md" src="#" alt="" />
-          </div>
-          <div className="flex flex-col justify-start text-center h-1/2">
-            <h1 className="text-3xl my-5">Card Title</h1>
-            <p className="text-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-        </div>
-        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
-          <button
-            className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="button"
-          >
-            Find Out more!
-          </button>
-        </div>
-      </div>
-      <div className="hover:scale-105 mx-10 my-5">
-        <div className="bg-white h-[450px] w-[325px] border rounded-t-lg">
-          <div className="h-1/2 bg-gray-400 rounded-t-lg">
-            <img className="rounded-t-md" src="#" alt="" />
-          </div>
-          <div className="flex flex-col justify-start text-center h-1/2">
-            <h1 className="text-3xl my-5">Card Title</h1>
-            <p className="text-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-          </div>
-        </div>
-        <div className="flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
-          <button
-            className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            type="button"
-          >
-            Find Out more!
-          </button>
-        </div>
+      <div className=" bg-white flex h-[50px] w-[325px] justify-center border-x border-b rounded-b-lg">
+        <button
+          className="my-2 bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          type="button"
+        >
+          Find Out more!
+        </button>
       </div>
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,48 @@ import { Jumbotron } from "@/components/Jumbotron";
 import { Card } from "@/components/Card";
 import { Footer } from "@/components/Footer";
 export default function Home() {
+  const cards = [
+    {
+      title: "Binky McSprinkles",
+      bio: "Binky is a professional bubble wrap popper and unicorn whisperer. In their free time, they enjoy playing hopscotch with chickens.",
+      imageURL:
+        "https://media.istockphoto.com/id/139975548/photo/sam.jpg?s=612x612&w=0&k=20&c=iFd7w-yqN5pkU_fAFImKTM6TJb9uRbHPKD1LQr8aO_A=",
+    },
+    {
+      title: "Chuckles von Schnitzel",
+      bio: "Chuckles is a world-renowned cheese sculptor and pogo stick enthusiast. They once broke the world record for most consecutive cartwheels.",
+      imageURL:
+        "https://i.insider.com/58d93bb6d349f92a008b734a?width=1000&format=jpeg&auto=webp",
+    },
+    {
+      title: "Flapjack McFluffernutter",
+      bio: "Flapjack is a part-time pirate and full-time pancake artist. They can juggle six flaming marshmallows without batting an eyelash.",
+      imageURL:
+        "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/11/27/1417095087495/bc71d79d-67bc-48a5-9d0f-834f43de6f98-1694x2040.jpeg?width=300&quality=45&auto=format&fit=max&dpr=2&s=b54ae0638a9a24874f3b551954efd71f",
+    },
+    {
+      title: "Noodles von Wigglesworth",
+      bio: "Noodles is a professional mermaid and ice cream taster. She once swam across the Atlantic Ocean while juggling pineapples.",
+      imageURL:
+        "https://1.bp.blogspot.com/-h-t1UYRXmts/XhfyGaC9FqI/AAAAAAACNSk/YmDw52WeWGE_5CDltqc8gG4YcYXzPSYhQCLcBGAsYHQ/s1600/1900s-female-bathing-suits-4.jpg",
+    },
+  ];
+
   return (
     <div className="bg-white">
-      <Navbar/>
+      <Navbar />
       <Jumbotron />
-      <Card />
+      <div className=" flex flex-wrap justify-center">
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            bio={card.bio}
+            imageURL={card.imageURL}
+          />
+        ))}
+      </div>
       <Footer />
-     
     </div>
   );
 }
